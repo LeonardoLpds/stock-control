f62c0cb3df2f219a9cccba7a7b3d3fc04eb59241
Leonardo de Souza <leonardo.lpds@gmail.com>
2016-02-06 16:20:18 -0200

    Creating constructView method in controller

--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -1,12 +1,16 @@
 <?php
 namespace app\controller;
+
 use app\model;
+use app\view\View;
+
 
 class Controller
 {
     //Properties
     private $model;
     private $action;
+    private $view;
     
     //Construct
     function __construct()
@@ -14,5 +18,15 @@ class Controller
         $urlParams = explode("/", substr($_SERVER['REQUEST_URI'], 1));
         $this->model = isset($urlParams[0]) ? $urlParams[0] : null;
         $this->action = isset($urlParams[1]) ? $urlParams[1] : null;
+        
+        $this->constructPage();
+    }
+
+    private function constructPage()
+    {
+        $this->view = new View();
+        $path = isset($this->model) ? $this->model : "/";
+        $page = isset($this->action) ? $this->action : "home";
+        $this->view->constructView($path.DIRECTORY_SEPARATOR.$page);
     }
 }
