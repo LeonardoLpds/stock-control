35eed1a3284279b67a3d36ebedb1d024c5e2f03a
Leonardo de Souza <leonardo.lpds@gmail.com>
2016-02-06 17:04:41 -0200

    Creating list page of products

---
+++ app/view/product/list.php
@@ -0,0 +1,28 @@
+<div class="page-header">
+    <h1>Produtos
+        <a class="btn btn-success pull-right" href="?view=forms/addProduto" role="button">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Adicionar Produto
+        </a>
+    </h1>
+    
+</div>
+<table class="table table-hover">
+    <thead>
+    <tr><th>Nome</th><th>Descrição</th><th>Preço</th><th></th></tr></thead>
+        <tr>
+            <th>nome</th>
+            <td>descricao</td>
+            <td>preco</td>
+            <td class="col-md-1 text-center">
+                <a href="" data-toggle="tooltip" data-placement="top" title="Editar">
+                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                </a>
+                <a href="" data-toggle="tooltip" data-placement="top" title="Excluir">
+                    <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+                </a>
+                <a href="" data-toggle="tooltip" data-placement="top" title="Fazer Pedido">
+                    <span class="glyphicon glyphicon-paste" aria-hidden="true"></span>
+                </a>
+            </td>
+        </tr>
+</table>
