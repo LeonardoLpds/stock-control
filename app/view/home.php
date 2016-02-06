3597232e61090b039b904b499d1bb011aa7f14fb
Leonardo de Souza <leonardo.lpds@gmail.com>
2016-02-06 15:54:27 -0200

    Creating view home

---
+++ app/view/home.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="page-header">
+          <h1>Stock Control <small>Test - Altran Brazil</small></h1>
+        </div>
+    </div>
+</div>
